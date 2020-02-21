6edfe19d02b50b08fe7b101d04e36555b2d88e52
Kartikey <parasharkartikey@gmail.com>
2020-02-21 13:12:23 +0530

    Added branch UC4

--- snake_and_ladder_game.sh
+++ snake_and_ladder_game.sh
@@ -1,8 +1,6 @@
 echo "...............................................WELCOME to SNAKE and LADDER game........................................................."
 echo "Let's start the game"
-
 declare -A randomCheck
-
 player_1_start_pos=0
 
 pos=$player_1_start_pos
@@ -10,6 +8,7 @@ pos=$player_1_start_pos
 NO_PLAY=1
 LADDER=2
 SNAKE=3
+WIN_POS=100
 
 function roll()
 {
@@ -20,20 +19,28 @@ function roll()
 
 function check_options()
 {
-   option=$(( RANDOM % 3 ))
+   option=$((RANDOM%3 + 1))
    case $option in
    $NO_PLAY)
-         echo "No Play"
          pos=$pos
          ;;
    $LADDER)
-         echo "Ladder"
-         pos=$(( $pos + $randomCheck ))
+			pos=$(( $pos + $randomCheck ))
          ;;
    $SNAKE)
          echo "Snake"
          pos=$(( $pos - $randomCheck ))
+         if [ $pos -lt $player_1_start_pos ]
+         then
+            pos=$player_1_start_pos
+         fi
          ;;
    esac
 }
 
+
+while [ $pos -lt $WIN_POS ]
+do
+   roll
+   check_options
+done
