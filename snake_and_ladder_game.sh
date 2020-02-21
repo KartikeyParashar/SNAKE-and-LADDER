74b82d0c659c42059c12a0a2b0dd2a9e3dc5e30a
Kartikey <parasharkartikey@gmail.com>
2020-02-21 12:39:40 +0530

    Added the UC2 branch

--- snake_and_ladder_game.sh
+++ snake_and_ladder_game.sh
@@ -1,3 +1,11 @@
 echo "...............................................WELCOME to SNAKE and LADDER game........................................................."
 echo "Let's start the game"
 
+
+function roll()
+{
+   randomCheck=$((RANDOM%6 + 1))
+   echo "When we throw the die $randomCheck comes"
+   ((totalrandomCheck++))
+}
+
